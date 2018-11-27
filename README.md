435ecdd86326016e1985f5fdc4b568cc7548fb3b
wjj <474957860@qq.com>
2018-11-27 15:28:07 +0800

    Vue+ElementUI的项目模版初始化

---
+++ README.md
@@ -0,0 +1,29 @@
+# 安装vue-cli
+
+    $ npm install -g @vue/cli  或者  yarn global add @vue/cli
+
+## 安装init桥接工具
+    npm install -g @vue/cli-init  或者  yarn global add @vue/cli-init
+
+
+# 安装该模版
+
+模版支持选项
+- project-name 项目名称
+
+命令行安装
+
+    $ vue init ImPigerla/template-vue-element [project-name]
+
+# 初始化
+    $ cd project-name
+    $ npm install 或者 yarn
+
+# 开发模式
+    $ npm run dev
+
+# 构建项目
+    $ npm run build
+
+# 构建监听
+    $ npm run watch
---
+++ template/src/views/app.vue
@@ -0,0 +1,24 @@
+<template>
+    <section>
+        <p class="text test-p">hello, Vue ElementUI App</p>
+        <el-input type="textarea" v-model.trim="content" placeholder="请输入"></el-input>
+        <p v-text="content"></p>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'app',
+        data () {
+            return {
+                content: ''
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    .test-p {
+        text-align: center;
+    }
+</style>
